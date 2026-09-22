5d659a8e190507196cc8203415917f2b86732025
linuan1026 <linuan1026@gmail.com>
2026-09-23 04:52:06 +0800

    Colab compatibility: use public PyTorch LRScheduler when available

--- dmpnn/chemprop/train/train.py
+++ dmpnn/chemprop/train/train.py
@@ -5,7 +5,10 @@ from tensorboardX import SummaryWriter
 import torch
 import torch.nn as nn
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
+try:
+    from torch.optim.lr_scheduler import LRScheduler as _LRScheduler
+except ImportError:  # PyTorch < 2.0
+    from torch.optim.lr_scheduler import _LRScheduler
 from tqdm import tqdm
 
 from chemprop.args import TrainArgs
